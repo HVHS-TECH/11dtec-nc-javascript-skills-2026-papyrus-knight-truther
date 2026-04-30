cece8569e04b1ad1c7b9566acaca69a46c16554b
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-04-30 21:33:46 +0000

    Apr 30, 2026, 9:33 PM

--- t03_maths.js
+++ t03_maths.js
@@ -8,7 +8,7 @@ let year=2025;
 let age=15;
 let money=10;
 let birthYear= year - age;
-let halfMoney;
+let halfMoney= money / 2;
 
 /*****************************
  Main code
@@ -17,9 +17,10 @@ console.log("Hi " + username + " as of " + year + " you are " + age + "years old
 console.log("You were born in " + birthYear )
 console.log("In 10 years you will be " + (age + 10) + " years old")
 console.log("You have " + money + " dollars")
-console.log("You spend half of your money, now you have " + (money / 2)) 
+console.log("You spend half of your money, now you have " + halfMoney)
+console.log("Then you get $3, now you have " + (halfMoney + 3)) 
 
-Then you get $3, now you have __
+
 
  /**************************
   Functions
