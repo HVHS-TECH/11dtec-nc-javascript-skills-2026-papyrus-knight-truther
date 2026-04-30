42dd47dcfaf530b6e131c72ecbdab8ecd39bb598
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-04-30 21:24:13 +0000

    Apr 30, 2026, 9:24 PM

--- t03_maths.js
+++ t03_maths.js
@@ -4,14 +4,16 @@ Name of task:maths
 
 //variables
 let username ="papyrusknighttruther" ;
-let year="2026";
-let age="15";
+let year=2026;
+let age=15;
 let birthYear;
 let halfMoney;
+
 /*****************************
  Main code
  ****************************/
-console.log("Hi " + username + " as of " + year + " you are " + age + "years old");
+console.log("Hi " + username + " as of " + year + " you are " + age + "years old")
+console.log("You were born in " + year - age )
 
  /**************************
   Functions
