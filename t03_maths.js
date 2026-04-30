42da6205b3796ca27fcd36b79da4337358c81347
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-04-30 21:30:44 +0000

    Apr 30, 2026, 9:30 PM

--- t03_maths.js
+++ t03_maths.js
@@ -6,6 +6,7 @@ Name of task:maths
 let username ="papyrusknighttruther" ;
 let year=2025;
 let age=15;
+let money=10;
 let birthYear= year - age;
 let halfMoney;
 
@@ -15,6 +16,10 @@ let halfMoney;
 console.log("Hi " + username + " as of " + year + " you are " + age + "years old")
 console.log("You were born in " + birthYear )
 console.log("In 10 years you will be " + (age + 10) + " years old")
+console.log("You have " + money + " dollars")
+console.log("You spend half of your money, now you have " + (money / 2)) 
+
+Then you get $3, now you have __
 
  /**************************
   Functions
