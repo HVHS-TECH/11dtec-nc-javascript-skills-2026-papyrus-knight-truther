4c9ff1cc214fbc359e3a47673ab045e141c55458
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-05-03 22:25:26 +0000

    May 3, 2026, 10:25 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,27 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task:maths
+****************************/
+
+//variables
+let username ="papyrusknighttruther" ;
+let year=2025;
+let age=15;
+let money=10;
+let birthYear= year - age;
+let halfMoney= money / 2;
+
+/*****************************
+ Main code
+ ****************************/
+console.log("Hi " + username + " as of " + year + " you are " + age + " years old")
+console.log("You were born in " + birthYear )
+console.log("In 10 years you will be " + (age + 10) + " years old")
+console.log("You have " + money + " dollars")
+console.log("You spend half of your money, now you have " + halfMoney)
+console.log("Then you get $3, now you have " + (halfMoney + 3) + " dollars") 
+
+
+
+ /**************************
+  Functions
+  *************************/
