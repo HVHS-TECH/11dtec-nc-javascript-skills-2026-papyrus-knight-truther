350e83c002a9a60d180686e5a72c5645500f64e4
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-05-04 23:36:16 +0000

    May 4, 2026, 11:36 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,31 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of task:functions
+****************************/
+
+console.log("running t05_avascriptsandhtml")
+
+//variables
+let username ="papyrusknighttruther" ;
+let year=2025;
+let age=15;
+let money=10;
+let birthYear= year - age;
+let halfMoney= money / 2;
+
+/*****************************
+ Main code
+ ****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+OUTPUT.innerHTML = "<h2>Woah javascript exists guys</h2>";
+OUTPUT.innerHTML += ("Hi " + username + " as of " + year + " you are " + age + " years old.")
+OUTPUT.innerHTML += (" You were born in " + birthYear )
+OUTPUT.innerHTML += ("In 10 years you will be " + (age + 10) + " years old. ")
+OUTPUT.innerHTML += ("You have " + money + " dollars. ")
+OUTPUT.innerHTML += ("You spend half of your money, now you have " + halfMoney)
+OUTPUT.innerHTML += (" Then you get $3, now you have " + (halfMoney + 3) + " dollars") 
+
+
+
+ /**************************
+  Functions
+  *************************/
