efa3bd2d94cb5597afff14f0c2ad96bb1572ab5e
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-05-05 00:08:33 +0000

    May 5, 2026, 12:08 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -5,17 +5,19 @@ Name of task:functions
 console.log("running t05_avascriptsandhtml")
 
 //variables
+/*
 let username ="papyrusknighttruther" ;
 let year=2025;
 let age=15;
 let money=10;
 let birthYear= year - age;
 let halfMoney= money / 2;
+*/
 
 /**************************
   Functions
   *************************/
-/* function writeLine(_name, _year, _age, _money, _birthYear, _halfMoney){
+ function writeLine(_name, _year, _age, _money, _birthYear, _halfMoney){
 OUTPUT.innerHTML = "<h2>WOAH ITS A FUNCTION </h2>";
 OUTPUT.innerHTML += ("Hi " + _name + " as of " + _year + " you are " + _age + " years old.")
 OUTPUT.innerHTML += (" You were born in " + _birthYear )
@@ -24,7 +26,7 @@ OUTPUT.innerHTML += ("You have " + _money + " dollars. ")
 OUTPUT.innerHTML += ("You spend half of your money, now you have " + _halfMoney)
 OUTPUT.innerHTML += (" Then you get $3, now you have " + (_halfMoney + 3) + " dollars") 
  }
-*/
+
 
 /*****************************
  Main code
