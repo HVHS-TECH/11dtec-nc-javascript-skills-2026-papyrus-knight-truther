3c6d93ead33b7db4c8dcea01d38bd139f4825212
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-05-10 22:47:12 +0000

    May 10, 2026, 10:47 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,57 @@
-/* Paste your code from the last task */
+/****************************
+Name of task:functions return
+****************************/
+
+console.log("running t13_functions returns")
+
+//variables
+
+let username = nameField.value;
+let year=2025;
+let age= ageField.value;
+let money=10;
+let birthYear= year - age;
+let halfMoney= money / 2;
+
+
+/**************************
+  Functions
+  *************************/
+ function writeLine(){
+OUTPUT.innerHTML += "<h2>WOAH ITS A FUNCTION </h2>";
+ }
+
+ 
+
+
+/*****************************
+ Main code
+ ****************************/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+function getFormInput(){
+const NAME_FIELD = document.getElementById("nameField")
+OUTPUT.innerHTML = "<p>Your name is "+ username +"</p>";
+}
+
+function getFormInputAge(){
+const AGE_FIELD = document.getElementById("ageField")
+OUTPUT.innerHTML = "<p>Your age is "+ age +"</p>";
+}
+
+function start(){
+const NAME_FIELD = document.getElementById("nameField")
+let username = nameField.value;
+    OUTPUT.innerHTML += ("<p>Hello "+ username +"</p>")
+ 
+const AGE_FIELD = document.getElementById("ageField")
+let age = ageField.value;
+    OUTPUT.innerHTML += ("<p>You are "+ age +"</p>")
+ }
+
+writeLine('papyrusknighttruther');
+
+
+
+
+ 
