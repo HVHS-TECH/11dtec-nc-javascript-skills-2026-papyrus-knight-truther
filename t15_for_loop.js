ab3ee62835b18cdc265486f65ab79f64fc560218
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-05-12 00:24:31 +0000

    May 12, 2026, 12:24 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -48,8 +48,8 @@ let pay = calculatePay(8);
 
 function start(){
     
-    for(let beer=5; beer=1; beer--){
-    OUTPUT.innerHTML += ("<p>" + beer + " bottles of beer on the wall! " + beer + " bottles of beer on the wall, " + beer + " bottles of beer! Take one down pass it around")
+    for(let beer=5; beer>1; beer--){
+    console.log += ("<p>" + beer + " bottles of beer on the wall! " + beer + " bottles of beer on the wall, " + beer + " bottles of beer! Take one down pass it around")
 }
 const NAME_FIELD = document.getElementById("nameField")
 let username = nameField.value;
