e7c4cae8a7a815bc80afac5e1e96cbd98ff2c901
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-05-25 23:42:18 +0000

    May 25, 2026, 11:42 PM

--- t22_the_DOM.js
+++ t22_the_DOM.js
@@ -1,1 +1,101 @@
-/* Paste your code from the last task */
+/****************************
+Name of task:functions return
+****************************/
+
+console.log("running t13_functions returns")
+
+//variables
+let user ={
+username: nameField.value,
+age: ageField.value
+}
+let hours=20;
+let first3Abc = ["a", "b", "c"];
+let shoppingList = ["Eggs", "Milk"];
+let year=2025;
+
+
+/**************************
+  Functions
+  *************************/
+
+ function calculatePay(hours){
+ let pay =  hours * 18.50;
+ pay = (pay * 0.105); //taxes :(
+ return pay;
+ }
+
+ 
+
+
+
+
+/*****************************
+ Main code
+ ****************************/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+function getFormInput(){
+const NAME_FIELD = document.getElementById("nameField")
+}
+
+function getFormInputAge(){
+const AGE_FIELD = document.getElementById("ageField")
+}
+
+function getFormInputShopping(){
+const SHOPPING_FIELD = document.getElementById("shoppingField")
+}
+
+function clearField(){
+ 
+ }
+ 
+calculatePay(hours);
+
+let pay = calculatePay(8);
+
+function start(){
+    
+    for(let beer=5; beer>1; beer--){
+    console.log( beer + " bottles of beer on the wall! " + beer + " bottles of beer on the wall, " + beer + " bottles of beer! Take one down pass it around")
+    
+}
+OUTPUT.innerHTML += ("<p> Shopping list:"+shoppingList+"</p>")
+const SHOPPING_FIELD = document.getElementById("shoppingField")
+shoppingList.push(shoppingField.value)
+    OUTPUT.innerHTML += ("<p>Shopping list: "+shoppingList+"</p>")
+
+const NAME_FIELD = document.getElementById("nameField")
+
+    if(NAME_FIELD.checkValidity()=== false) {
+        OUTPUT.innerHTML = "please fill out this form"
+    }
+    else {
+       let username = nameField.value;
+    OUTPUT.innerHTML += ("<p>Hello "+ username +"</p>") 
+    }
+ 
+const AGE_FIELD = document.getElementById("ageField")
+let age = ageField.value;
+if(age > 15 ){
+    OUTPUT.innerHTML += ("<p>You are an unc</p>")
+    OUTPUT.innerHTML += ("<p>You made $" + pay + "</p>")
+ }
+else
+OUTPUT.innerHTML += ("<p>You are a teeny child infant</p>")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
